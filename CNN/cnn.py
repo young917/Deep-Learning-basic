dc9201cea248f7ade9ae7667969ad79c45792f04
young917 <30820487+young917@users.noreply.github.com>
2020-03-05 19:31:26 +0900

    complete

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -43,14 +43,18 @@ class CNN_B():
 class CNN_C():
     def __init__(self):
         # Your initialization code goes here
-        self.layers = []
+        self.layers = [Conv1D(24, 2, 2, 2), ReLU(), Conv1D(2, 8, 2, 2), ReLU(), Conv1D(8, 4, 2, 1), Flatten()]
 
     def __call__(self, x):
         return self.forward(x)
 
     def init_weights(self, weights):
-        # Load the weights for your CNN from the MLP Weights given
-        raise NotImplemented
+        channels = [24, 2, 8, 4]
+        for w, idx in zip(weights, range(0, 3)):
+            arr = np.array([w.T[c, : 2*channels[idx]] for c in range(0, channels[idx+1])])
+            arr = arr.reshape((channels[idx+1], 2, channels[idx]))
+            self.layers[2*idx].W = np.transpose(arr, (0, 2, 1))
+
 
     def forward(self, x):
         # You do not need to modify this method
